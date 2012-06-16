0b6b7f25713fdb6fc8b6a8c466da06b1f8c7cab2
Ash Berlin <ash_github@firemirror.com>
2012-06-16 18:02:31 +0100

    App cookbook requires nginx

--- cookbooks/application/metadata.rb
+++ cookbooks/application/metadata.rb
@@ -7,15 +7,8 @@ version          "0.99.14"
 recipe           "application", "Loads application databags and selects recipes to use"
 recipe           "application::django", "Deploys a Django application specified in a data bag with the deploy_revision resource"
 recipe           "application::gunicorn", "Sets up the deployed Django application with Gunicorn as the web server"
-recipe           "application::java_webapp", "Deploys a Java web application WAR specified in a data bag with the remote_file resource"
-recipe           "application::mod_php_apache2", "Sets up a deployed PHP application as a mod_php virtual host in Apache2"
-recipe           "application::passenger_apache2", "Sets up a deployed Rails application as a Passenger virtual host in Apache2"
-recipe           "application::php", "Deploys a PHP application specified in a data bag with the deploy_revision resource"
-recipe           "application::rails", "Deploys a Rails application specified in a data bag with the deploy_revision resource"
-recipe           "application::tomcat", "Sets up the deployed Java application with Tomcat as the servlet container"
-recipe           "application::unicorn", "Sets up the deployed Rails application with Unicorn as the web server"
 
-%w{ gunicorn }.each do |cb|
+%w{ gunicorn nginx }.each do |cb|
   depends cb
 end
 
