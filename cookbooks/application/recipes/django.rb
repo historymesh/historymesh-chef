0e1566d3fec871bfdc7e4cba72d4e8e41ac28fba
Ash Berlin <ash_github@firemirror.com>
2012-06-16 17:48:44 +0100

    Deployed and runs now \o/

--- cookbooks/application/recipes/django.rb
+++ cookbooks/application/recipes/django.rb
@@ -28,20 +28,9 @@ include_recipe "python"
 
 node.default[:apps][app['id']][node.chef_environment][:run_migrations] = false
 
-# the Django split-settings file name varies from project to project...+1 for standardization
-local_settings_full_path = app['local_settings_file'] || 'settings_local.py'
-local_settings_file_name = local_settings_full_path.split(/[\\\/]/).last
-
 ## Create required directories
 
-directory app['deploy_to'] do
-  owner app['owner']
-  group app['group']
-  mode '0755'
-  recursive true
-end
-
-directory "#{app['deploy_to']}/shared" do
+directory "#{app['deploy_to']}/shared/config" do
   owner app['owner']
   group app['group']
   mode '0755'
@@ -113,34 +102,51 @@ if app["database_master_role"]
       dbm = rows[0]
     end
   end
-  
+
   # we need the django version to render the correct type of settings.py file
   django_version = 1.2
   if app['pips'].has_key?('django') && !app['pips']['django'].strip.empty?
     django_version = app['pips']['django'].to_f
   end
 
-  # Assuming we have one...
-  if dbm
-    # local_settings.py
-    template "#{app['deploy_to']}/shared/#{local_settings_file_name}" do
-      source "settings.py.erb"
-      owner app["owner"]
-      group app["group"]
-      mode "644"
-      variables(
-        :host => (dbm.attribute?('cloud') ? dbm['cloud']['local_ipv4'] : dbm['ipaddress']),
-        :database => app['databases'][node.chef_environment],
-        :django_version => django_version
-      )
-    end
-  else
-    Chef::Log.warn("No node with role #{app["database_master_role"][0]}, #{local_settings_file_name} not rendered!")
+  unless dbm
+    Chef::Log.warn("No node with role #{app["database_master_role"][0]}")
+  end
+end
+
+file "#{app['deploy_to']}/shared/config/__init__.py" do
+  owner app['owner']
+  group app['group']
+  mode "0644"
+  content ""
+end
+
+# Assuming we have one...
+if dbm
+  unless app['databases'][node.chef_environment].has_key? 'password'
+    passwords = Chef::EncryptedDataBagItem.load("passwords", "postgres")
+    app['databases'][node.chef_environment][ 'password' ] = passwords[ app['databases'][node.chef_environment]['username'] ]
+  end
+
+  # local_settings.py
+  template "#{app['deploy_to']}/shared/config/settings.py" do
+    source "settings.py.erb"
+    owner app["owner"]
+    group app["group"]
+    mode "644"
+    variables(
+      # TODO: restpose is currently hardcoded in the template to localhost
+      :db_master => (dbm == node ? '127.0.0.1' : dbm.attribute?('cloud') ? dbm['cloud']['local_ipv4'] : dbm['ipaddress']),
+      :database => app['databases'][node.chef_environment],
+      :django_version => django_version
+    )
   end
 end
 
 ## Then, deploy
 deploy_revision app['id'] do
+  # provider HistoryMesh::Antler::Deploy::Revision
+
   revision app['revision'][node.chef_environment]
   repository app['repository']
   user app['owner']
@@ -152,15 +158,21 @@ deploy_revision app['id'] do
   purge_before_symlink([])
   create_dirs_before_symlink([])
   symlinks({})
+  # Done in the before_migrate block instead
+  symlink_before_migrate({})
+
+  environment "DJANGOENV" => "production"
   before_migrate do
     requirements_file = nil
     # look for requirements.txt files in common locations
-    if ::File.exists?(::File.join(release_path, "requirements", "#{node[:chef_environment]}.txt"))
+    if app.has_key?( 'requirements_file' )
+      requirements_file = ::File.join(release_path, app['requirements_file'])
+    elsif ::File.exists?(::File.join(release_path, "requirements", "#{node[:chef_environment]}.txt"))
       requirements_file = ::File.join(release_path, "requirements", "#{node.chef_environment}.txt")
     elsif ::File.exists?(::File.join(release_path, "requirements.txt"))
       requirements_file = ::File.join(release_path, "requirements.txt")
     end
-    
+
     if requirements_file
       Chef::Log.info("Installing pips using requirements file: #{requirements_file}")
       pip_cmd = File.join(ve.path, "bin", "pip")
@@ -169,15 +181,20 @@ deploy_revision app['id'] do
         cwd release_path
       end
     end
-  end
 
-  symlink_before_migrate({
-    local_settings_file_name => local_settings_full_path
-  })
+    shared_path = @new_resource.shared_path
+    link "#{release_path}/antler/configs/production" do
+      to "#{shared_path}/config"
+    end
+    # This shouldn't be needed but the manage.py/django is doing something funny
+    link "#{release_path}/antler/production" do
+      to "#{shared_path}/config"
+    end
+  end
 
   if app['migrate'][node.chef_environment] && node[:apps][app['id']][node.chef_environment][:run_migrations]
     migrate true
-    migration_command app['migration_command'] || "#{::File.join(ve.path, "bin", "python")} manage.py migrate"
+    migration_command app['migration_command'] || "#{::File.join(ve.path, "bin", "python")} antler/manage.py migrate"
   else
     migrate false
   end
@@ -190,5 +207,33 @@ deploy_revision app['id'] do
         end
       end
     end
+
+    execute "cssprepare" do
+      command "cssprepare --optimise --extended-syntax antler/static/css/screen > antler/static/css/screen.css"
+      user app['owner']
+      cwd release_path
+    end
+
+    execute "jsprepare" do
+      command "jsprepare antler/static/js/common/control.js > antler/static/js/common.js"
+      user app['owner']
+      cwd release_path
+    end
   end
 end
+
+template "/etc/nginx/sites-available/historymesh.com" do
+  source "django_nginx.conf.erb"
+  backup false
+  mode "0644"
+  variables(
+    :app => app['id'],
+    :server_name => 'historymesh.com',
+    :server_aliases => 'www.historymesh.com',
+    :docroot => "#{app['deploy_to']}/current/antler/static",
+    :django_admin_static_root => "#{ve.path}/lib/#{ve.interpreter}/site-packages/django/contrib/admin/"
+  )
+  notifies :restart, "service[nginx]"
+end
+
+nginx_site "historymesh.com"
--- cookbooks/application/recipes/gunicorn.rb
+++ cookbooks/application/recipes/gunicorn.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: application
-# Recipe:: gunicorn 
+# Recipe:: gunicorn
 #
 # Copyright 2011, Opscode, Inc.
 #
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-app = node.run_state[:current_app] 
+app = node.run_state[:current_app]
 
 ve = resources(:python_virtualenv => app['id'])
 node.default[:gunicorn][:virtualenv] = ve.path
@@ -27,18 +27,18 @@ include_recipe "gunicorn"
 node.default[:gunicorn][:worker_timeout] = 60
 node.default[:gunicorn][:preload_app] = false
 node.default[:gunicorn][:worker_processes] = [node[:cpu][:total].to_i * 4, 8].min
-node.default[:gunicorn][:server_hooks] = {:pre_fork => 'import time;time.sleep(1)'}
+# node.default[:gunicorn][:server_hooks] = {:pre_fork => 'import time;time.sleep(1)'}
 node.default[:gunicorn][:port] = '8080'
 
 gunicorn_config "/etc/gunicorn/#{app['id']}.py" do
   listen "#{node[:ipaddress]}:#{node[:gunicorn][:port]}"
-  worker_timeout node[:gunicorn][:worker_timeout] 
-  preload_app node[:gunicorn][:preload_app] 
+  worker_timeout node[:gunicorn][:worker_timeout]
+  preload_app node[:gunicorn][:preload_app]
   worker_processes node[:gunicorn][:worker_processes]
   server_hooks node[:gunicorn][:server_hooks]
   action :create
 end
- 
+
 runit_service app['id'] do
   template_name 'gunicorn'
   cookbook 'application'
